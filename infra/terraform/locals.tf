b9a43e0f2fde1f61729c1bcb49e9e2f7c7ede7c3
Julian Butler <Julian.Butler@mainecybertech.com>
2026-06-21 19:54:12 -0400

    chore: fix terraform fmt indentation in locals.tf

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -2,7 +2,7 @@ locals {
   domain_names = var.environment == "production" ? {
     frontend = "chat.mainecybertech.com"
     api      = "chat-api.mainecybertech.com"
-  } : {
+    } : {
     frontend = "chat.mainecybertech.us"
     api      = "chat-api.mainecybertech.us"
   }
