fbbd2e3b52b580b439294b5a817ebddd2f0d5671
Julian Butler <Julian.Butler@mainecybertech.com>
2026-06-24 00:52:31 -0400

    fix: remove endpoint from versions.tf, use full URL in workflow

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -4,9 +4,6 @@ terraform {
   backend "s3" {
     bucket                      = "chat-terraform-state"
     key                         = "infra/terraform.tfstate"
-    endpoints = {
-      s3 = "https://nyc3.digitaloceanspaces.com"
-    }
     region                      = "us-east-1"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
