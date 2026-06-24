927b5ac0f9643239c962c5dc62e72c9ff8761202
Julian Butler <Julian.Butler@mainecybertech.com>
2026-06-24 01:16:02 -0400

    fix: update terraform backend region to sfo3

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket                      = "chat-terraform-state"
     key                         = "infra/terraform.tfstate"
-    region                      = "us-east-1"
+    region                      = "sfo3"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     encrypt                     = true
